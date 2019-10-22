2692670b33cdfb331b337cff148d0a86c4d3a175
yyy9942 <yyy9942@navere.com>
2019-10-22 20:56:13 +0900

    매장 조회 기능 추가 1. 페이징 적용 - page 값을 주지 않을시 자동으로 0 값을 주입
    2. 총 매장 개수를 같이 return하여 매장 개수를 알 수 있도록 만듦

--- src/main/java/com/delfood/controller/ShopController.java
+++ src/main/java/com/delfood/controller/ShopController.java
@@ -1,20 +1,25 @@
 package com.delfood.controller;
 
-import com.delfood.dto.ShopDTO;
-import com.delfood.service.ShopService;
-import com.delfood.utils.SessionUtil;
+import java.util.List;
 import javax.servlet.http.HttpSession;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.delfood.dto.ShopDTO;
+import com.delfood.service.ShopService;
+import com.delfood.utils.SessionUtil;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping("/owners/shops/")
@@ -32,13 +37,13 @@ public class ShopController {
    * @return
    */
   @PostMapping
-  public ResponseEntity<OpenShopResponse> openShop(HttpSession session,
+  public ResponseEntity<AddShopResponse> addShop(HttpSession session,
       @RequestBody ShopDTO shopInfo) {
     String ownerId = SessionUtil.getLoginOwnerId(session);
 
     // 로그인 하지 않았을 시 401코드를 반환한다.
     if (ownerId == null) {
-      return new ResponseEntity<ShopController.OpenShopResponse>(OpenShopResponse.NO_LOGIN,
+      return new ResponseEntity<ShopController.AddShopResponse>(AddShopResponse.NO_LOGIN,
           HttpStatus.UNAUTHORIZED);
     }
 
@@ -46,15 +51,36 @@ public class ShopController {
 
     // 입력한 데이터 중 필수 데이터가 null일 경우 400 에러코드를 반환한다.
     if (ShopDTO.hasNullDataBeforeCreate(shopInfo)) {
-      return new ResponseEntity<ShopController.OpenShopResponse>(OpenShopResponse.NULL_ARGUMENTS,
+      return new ResponseEntity<ShopController.AddShopResponse>(AddShopResponse.NULL_ARGUMENTS,
           HttpStatus.BAD_REQUEST);
     }
 
     shopService.addShop(shopInfo);
 
 
-    return new ResponseEntity<ShopController.OpenShopResponse>(OpenShopResponse.SUCCESS,
+    return new ResponseEntity<ShopController.AddShopResponse>(AddShopResponse.SUCCESS,
         HttpStatus.CREATED);
+  }
+
+  /**
+   * 사장님이 가진 매장들을 불러온다.
+   * 
+   * @param myShopsRequest 페이징 정보
+   * @param session 사용자의 세션
+   * @return 페이지에 따른 사장님 매장, 총 매장 개수
+   */
+  @GetMapping
+  public ResponseEntity<MyShopsResponse> myShops(MyShopsRequest myShopsRequest,
+      HttpSession session) {
+    String id = SessionUtil.getLoginOwnerId(session);
+    if (id == null) {
+      return new ResponseEntity<MyShopsResponse>(MyShopsResponse.NO_LOGIN, HttpStatus.UNAUTHORIZED);
+    }
+
+    List<ShopDTO> myShops = shopService.getMyShops(id, myShopsRequest.getPage());
+    long myShopCount = shopService.getMyShopCount(id);
+    return new ResponseEntity<MyShopsResponse>(MyShopsResponse.success(myShops, myShopCount),
+        HttpStatus.OK);
   }
 
 
@@ -62,7 +88,7 @@ public class ShopController {
   // Response 객체
   @Getter
   @RequiredArgsConstructor
-  private static class OpenShopResponse {
+  private static class AddShopResponse {
     enum Result {
       SUCCESS, NO_LOGIN, NULL_ARGUMENTS
     }
@@ -70,10 +96,40 @@ public class ShopController {
     @NonNull
     Result result;
 
-    private static final OpenShopResponse SUCCESS = new OpenShopResponse(Result.SUCCESS);
-    private static final OpenShopResponse NO_LOGIN = new OpenShopResponse(Result.NO_LOGIN);
-    private static final OpenShopResponse NULL_ARGUMENTS =
-        new OpenShopResponse(Result.NULL_ARGUMENTS);
+    private static final AddShopResponse SUCCESS = new AddShopResponse(Result.SUCCESS);
+    private static final AddShopResponse NO_LOGIN = new AddShopResponse(Result.NO_LOGIN);
+    private static final AddShopResponse NULL_ARGUMENTS =
+        new AddShopResponse(Result.NULL_ARGUMENTS);
+  }
+
+  @Getter
+  @RequiredArgsConstructor
+  @AllArgsConstructor
+  private static class MyShopsResponse {
+    enum Result {
+      SUCCESS, NO_LOGIN
+    }
+
+    @NonNull
+    Result result;
+
+    List<ShopDTO> myShops;
+
+    Long shopCount;
+
+    private static final MyShopsResponse NO_LOGIN = new MyShopsResponse(Result.NO_LOGIN);
+
+    public static MyShopsResponse success(List<ShopDTO> myShops, Long shopCount) {
+      return new MyShopsResponse(Result.SUCCESS, myShops, shopCount);
+    }
+  }
+
+  // Request 객체
+  @Getter
+  @Setter
+  private static class MyShopsRequest {
+    @Nullable
+    private Long page;
   }
 
 }
--- src/main/java/com/delfood/service/ShopService.java
+++ src/main/java/com/delfood/service/ShopService.java
@@ -4,6 +4,7 @@ import com.delfood.dto.ShopDTO;
 import com.delfood.mapper.OperationResult;
 import com.delfood.mapper.ShopMapper;
 import lombok.extern.log4j.Log4j2;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,5 +26,24 @@ public class ShopService {
       log.error("insert ERROR - {}", shopInfo);
       throw new RuntimeException("Shop insert ERROR");
     }
+  }
+
+  /**
+   * 사장님 가게들의 정보를 불러오는 메서드.
+   * @param id 사장님 id
+   * @param page 페이지 정보
+   * @return
+   */
+  public List<ShopDTO> getMyShops(String id, Long page) {
+    return shopMapper.findByOwnerId(id, page);
+  }
+
+  /**
+   * 사장님이 가진 총 가게 개수를 불러오는 메서드.
+   * @param id 사장님 아이디
+   * @return
+   */
+  public long getMyShopCount(String id) {
+    return shopMapper.countByOwnerId(id);
   }
 }
