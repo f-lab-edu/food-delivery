4437bcd6cd7e60f99b982b8736609e38bd783300
Jun <yyy9942@naver.com>
2020-01-16 11:09:23 +0900

    리뷰 반영

--- src/main/java/com/delfood/dto/push/PushMessage.java
+++ src/main/java/com/delfood/dto/push/PushMessage.java
@@ -12,6 +12,14 @@ public class PushMessage {
   @NonNull
   private String message;
   
+  public static final PushMessage ADD_ORDER_REQUEST = new PushMessage("DelFood 주문", "새로운 주문이 들어왔습니다");
+  public static final PushMessage ACCEPT_ORDER_REQUEST = new PushMessage("DelFood 접수", "주문이 접수되었습니다");
+  public static final PushMessage REQUIRED_ORDER_REQUEST = new PushMessage("DelFood 주문취소", "매장에서 주문을 취소하였습니다");
+  public static final PushMessage DELIVERY_MATCH = new PushMessage("DelFood 배달원 매칭", "배달원이 매칭되었습니다");
+  public static final PushMessage DELIVERY_START = new PushMessage("DelFood 배달 시작", "음식 배달이 시작되었습니다");
+  public static final PushMessage DELIVERY_SUCCESS = new PushMessage("DelFood 배달 완료", "배달이 완료되었습니다");
+  
+  
   private LocalDateTime generatedTime;
   
   public PushMessage(String title, String message) {
@@ -21,20 +29,5 @@ public class PushMessage {
   }
   
   
-  public static PushMessage getMessasge(Type type) {
-    return type.pushMessage;
-  }
-  
-  @AllArgsConstructor
-  public static enum Type {
-    addOrderRequest(new PushMessage("DelFood 주문", "새로운 주문이 들어왔습니다")),
-    acceptOrderRequest(new PushMessage("DelFood 접수", "주문이 접수되었습니다")),
-    requiredOrderRequest(new PushMessage("DelFood 주문취소", "매장에서 주문을 취소하였습니다")),
-    deliveryMatch(new PushMessage("DelFood 배달원 매칭", "배달원이 매칭되었습니다")),
-    deliveryStart(new PushMessage("DelFood 배달 시작", "음식 배달이 시작되었습니다")),
-    deliverySuccess(new PushMessage("DelFood 배달 완료", "배달이 완료되었습니다"));
-    
-    private PushMessage pushMessage;
-  }
   
 }
