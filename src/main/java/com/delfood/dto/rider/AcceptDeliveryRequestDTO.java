82f0018e7b981baa8b0199832bce194f7678feb0
yyy99 <yyy99@DESKTOP-2TP3UBH>
2020-01-17 22:46:49 +0900

    maven에서 빌드되지 않는 버그 수정

--- src/main/java/com/delfood/dto/rider/AcceptDeliveryRequestDTO.java
+++ src/main/java/com/delfood/dto/rider/AcceptDeliveryRequestDTO.java
@@ -4,10 +4,10 @@ import java.time.LocalDateTime;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Getter
-@Builder
 public class AcceptDeliveryRequestDTO {
 
   @NonNull
@@ -23,9 +23,10 @@ public class AcceptDeliveryRequestDTO {
   
   
   @Builder
-  public AcceptDeliveryRequestDTO(Long orderId, String riderId) {
+  public AcceptDeliveryRequestDTO(Long orderId, String riderId, RequestResult result) {
     this.orderId = orderId;
     this.riderId = riderId;
+    this.result = result;
     startedAt = LocalDateTime.now();
   }
   
